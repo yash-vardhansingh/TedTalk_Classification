5c0d6df2cb8295b5451fda57a294067c1f07b714
yash-vardhansingh <156059267+yash-vardhansingh@users.noreply.github.com>
2024-02-15 11:34:37 +0530

    Add files via upload

--- flask app/app.py
+++ flask app/app.py
@@ -1,36 +1,43 @@
 from flask import Flask, request, render_template
+from transformers import BartForConditionalGeneration, BartTokenizer
 from fastai.text.all import load_learner
-from pathlib import Path, WindowsPath, PosixPath
-import pathlib
-from contextlib import contextmanager
 from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
 
-# Define the context manager to temporarily patch PosixPath
-@contextmanager
-def set_posix_to_windows_path():
-    original_posix_path = PosixPath
-    try:
-        pathlib.PosixPath = WindowsPath
-        yield
-    finally:
-        pathlib.PosixPath = original_posix_path
-
-model = None
+# Initialize global variables for summarization
+summarization_model = None
+tokenizer = None
 # Your model's path
-model_path = Path(r".\exported_learner.pkl")
+model = None
+model_path = "./exported_learner.pkl"
+
+def get_model():
+    global model
+    if model is None:
+        model = load_learner(model_path)
+    return model
+
+def load_summarization_model():
+    global summarization_model, tokenizer
+    summarization_model = BartForConditionalGeneration.from_pretrained("./new")
+    tokenizer = BartTokenizer.from_pretrained("./new")
+
+def load_model():
+    get_model()  # Load the Fastai model
+    load_summarization_model()  # Load the BART model and tokenizer
+
+
+def summarize_text(text_to_summarize):
+    inputs = tokenizer(text_to_summarize, return_tensors="pt", max_length=1024, truncation=True)
+    summary_ids = summarization_model.generate(inputs["input_ids"], max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary
 
 @app.route('/', methods=['GET'])
 def index():
     # Render the HTML form, adjusted to accept YouTube URLs
     return render_template('index.html')
-
-@app.before_first_request
-def load_model():
-    global model
-    with set_posix_to_windows_path():
-        model = load_learner(model_path)
 
 def extract_transcript_details(youtube_video_url):
     try:
@@ -43,19 +50,6 @@ def extract_transcript_details(youtube_video_url):
     except Exception as e:
         print(f"Error extracting transcript: {e}")
         return None
-"""
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        youtube_video_url = request.form.get('youtubeURL')
-        transcript = extract_transcript_details(youtube_video_url)
-        if transcript:
-            pred_class, _, _ = model.predict(transcript)
-            return render_template('result.html', predicted_class=pred_class)
-        else:
-            error_message = "Failed to extract transcript. Please check the YouTube URL and try again."
-            return render_template('error.html', error_message=error_message)
-"""
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -63,18 +57,23 @@ def predict():
         youtube_video_url = request.form.get('youtubeURL')
         transcript = extract_transcript_details(youtube_video_url)
         if transcript:
+            # Perform classification
+            model = get_model()
             pred_class, pred_idx, pred_probs = model.predict(transcript)
-            # Get the top 2 predictions and their probabilities
             top_preds = pred_probs.topk(2)
             top_classes = [model.dls.vocab[1][i] for i in top_preds.indices]
             top_probs = [round(p.item() * 100, 2) for p in top_preds.values]
-            # Pack the top predictions and their probabilities in a list of tuples
             predictions = list(zip(top_classes, top_probs))
-            return render_template('result.html', predictions=predictions)
+
+            # Perform summarization
+            summary = summarize_text(transcript)
+
+            return render_template('result.html', predictions=predictions, summary=summary)
         else:
             error_message = "Failed to extract transcript. Please check the YouTube URL and try again."
             return render_template('error.html', error_message=error_message)
 
+load_model()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False)
